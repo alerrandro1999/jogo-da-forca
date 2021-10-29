670591e625e5c5de884ddef635e76329c1ba631d
Alerrandro Borges <alerrandrokaton@gmail.com>
2021-10-29 13:36:36 -0300

    Site completo da forca

---
+++ src/components/Final.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="final">
+        <div :class="`final-texto ${etapa}`">{{texto}}</div>
+        <button v-on:click="jogarNovamente" class="final-botao">Jogar novamente</button>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+   name:'Final',
+   props:{
+       etapa: String,
+       texto:String,
+       jogarNovamente: Function
+   },
+   data(){
+       return{
+     
+       }
+   },
+   methods:{
+     
+   },
+   components:{
+      
+   }
+}
+</script>
+
+<style scoped>
+    .final-texto{
+        margin: 20px 0px;
+        font-size: 24px;
+        font-weight: bold;
+        color: white;
+    }
+
+    .final-texto.enforcado{
+        color: var(--color-text-error);
+    }
+
+    .final-texto.ganhador{
+        color: var(--color-text-success);
+    }
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="formulario">
+        <div class="formulario-title"><h1>{{title}}</h1></div>
+        <input type="text" class="formulario-input" v-model="inputValue">
+        <button :disabled="inputValue.length === 0"
+         v-on:click="onsubmit"
+        >{{button}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+   name:'Formulario',
+   props:{
+       title: String,
+       button: String,
+       action: Function
+   },
+   data(){
+       return{
+           inputValue:''
+       }
+   },
+   methods:{
+       onsubmit: function () {
+           this.action(this.inputValue);
+           this.inputValue = '';
+       }
+   }
+}
+</script>
+
+<style scoped>
+
+    .formulario{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
+        margin-top: 20px;
+    }
+
+    .formulario-input{
+        padding: 10px 5px;
+        border: 0;
+        border-radius: 5px;
+        margin: 10px 0px;
+        text-align: center;
+        color: var(--color-text-dark);
+        font-size: 16px;
+    }
+</style>
